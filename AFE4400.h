0dbd6c7b3bfc64abd674bf4f0c1d3487edf08789
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-03-26 16:10:22 +0200

    Added initial AFE4400 register map typedef

---
+++ AFE4400.h
@@ -0,0 +1,66 @@
+#ifndef AFE4400_H
+#define AFE4400_H
+
+
+/**
+ * AFE4400 register map
+*/
+typedef enum
+{
+    CONTROL0		 =	0x00,   /** Register short description - to do */
+    LED2STC			 =	0x01,   /** Register short description - to do */
+    LED2ENDC		 =	0x02,   /** Register short description - to do */
+    LED2LEDSTC		 =	0x03,   /** Register short description - to do */
+    LED2LEDENDC		 =	0x04,   /** Register short description - to do */
+    ALED2STC		 =	0x05,   /** Register short description - to do */
+    ALED2ENDC		 =	0x06,   /** Register short description - to do */
+    LED1STC			 =	0x07,   /** Register short description - to do */
+    LED1ENDC		 =	0x08,   /** Register short description - to do */
+    LED1LEDSTC		 =	0x09,   /** Register short description - to do */
+    LED1LEDENDC		 =	0x0A,   /** Register short description - to do */
+    ALED1STC		 =	0x0B,   /** Register short description - to do */
+    ALED1ENDC		 =	0x0C,   /** Register short description - to do */
+    LED2CONVST		 =	0x0D,   /** Register short description - to do */
+    LED2CONVEND		 =	0x0E,   /** Register short description - to do */
+    ALED2CONVST		 =	0x0F,   /** Register short description - to do */
+    ALED2CONVEND	 =	0x10,   /** Register short description - to do */
+    LED1CONVST		 =	0x11,   /** Register short description - to do */
+    LED1CONVEND		 =	0x12,   /** Register short description - to do */
+    ALED1CONVST		 =	0x13,   /** Register short description - to do */
+    ALED1CONVEND	 =	0x14,   /** Register short description - to do */
+    ADCRSTSTCT0		 =	0x15,   /** Register short description - to do */
+    ADCRSTENDCT0	 =	0x16,   /** Register short description - to do */
+    ADCRSTSTCT1		 =	0x17,   /** Register short description - to do */
+    ADCRSTENDCT1	 =	0x18,   /** Register short description - to do */
+    ADCRSTSTCT2		 =	0x19,   /** Register short description - to do */
+    ADCRSTENDCT2	 =	0x1A,   /** Register short description - to do */
+    ADCRSTSTCT3		 =	0x1B,   /** Register short description - to do */
+    ADCRSTENDCT3	 =	0x1C,   /** Register short description - to do */
+    PRPCOUNT		 =	0x1D,   /** Register short description - to do */
+    CONTROL1		 =	0x1E,   /** Register short description - to do */
+    SPARE1			 =	0x1F,   /** Register short description - to do */
+    TIAGAIN			 =	0x20,   /** Register short description - to do */
+    TIA_AMB_GAIN	 =	0x21,   /** Register short description - to do */
+    LEDCNTRL		 =	0x22,   /** Register short description - to do */
+    CONTROL2		 =	0x23,   /** Register short description - to do */
+    SPARE2		 	 =	0x24,   /** Register short description - to do */
+    SPARE3		 	 =	0x25,   /** Register short description - to do */
+    SPARE4		 	 =	0x26,   /** Register short description - to do */
+    RESERVED1		 =	0x27,   /** Register short description - to do */
+    RESERVED2		 =	0x28,   /** Register short description - to do */
+    ALARM		 	 =	0x29,   /** Register short description - to do */
+    LED2VAL		 	 =	0x2A,   /** Register short description - to do */
+    ALED2VAL		 =	0x2B,   /** Register short description - to do */
+    LED1VAL		 	 =	0x2C,   /** Register short description - to do */
+    ALED1VAL		 =	0x2D,   /** Register short description - to do */
+    LED2_ALED2VAL	 =	0x2E,   /** Register short description - to do */
+    LED1_ALED1VAL	 =	0x2F 	/** Register short description - to do */
+}AFE4400_REGS_ADDRESS_t
+
+
+
+
+
+#endif
+
+
