a8214b5e579f366bfe6fee302e6f932e9f8ca2c3
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-03-28 19:42:32 +0200

    Added ';' at the end of AFE4400_REGS_ADDRESS_t

--- AFE4400.h
+++ AFE4400.h
@@ -61,7 +61,7 @@ typedef enum
     ALED1VAL		 =	0x2D,   /** LED1 ambient digital value */
     LED2_ALED2VAL	 =	0x2E,   /** (LED2 – LED2 ambient) digital value */
     LED1_ALED1VAL	 =	0x2F 	/** (LED1 – LED1 ambient) digital value */
-}AFE4400_REGS_ADDRESS_t
+}AFE4400_REGS_ADDRESS_t;
 
 
 
