172283718582d528af1f3e7dca72dfc4b2343275
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-03-29 18:24:30 +0200

    Added DiagnosticEnable() definition

--- AFE4400.c
+++ AFE4400.c
@@ -17,5 +17,8 @@ static uint8_t IsSoftwareResetDone(AFE4400_Data_t *Data)
     return Data->CONTROL0 & SW_RST ? 0 : 1;
 }
 
-
+static void DiagnosticEnable(AFE4400_Data_t *Data)
+{
+    Data->CONTROL0 |= DIAG_EN;
+}
 
