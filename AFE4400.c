2369f9543b940a46a4dd0b36bd0504ab94cb0a4b
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-03-29 20:53:57 +0200

    Added ADCRST start&end counter equations in TimerModuleInit()

--- AFE4400.c
+++ AFE4400.c
@@ -17,7 +17,31 @@ static void DiagnosticEnable(AFE4400_Data_t *Data)
     Data->CONTROL0 |= DIAG_EN;
 }
 
-static void TimerModuleInit(uint8_t PRF , uint8_t DutyCycle, AFE4400_Data_t *Data)
+static void TimerModuleInit( uint16_t PRF , uint8_t DutyCycle, AFE4400_Data_t *Data)
 {
+   if (0 != PRF)
+   {
+        Data->PRPCOUNT = (uint16_t *)(AFE4400_CLOCK_FRQ / PRF - 1);
+        Data->ADCRSTENDCT0 = Data->PRPCOUNT;
 
+
+        if ((0 < DutyCycle) && (100 > DutyCycle))
+        {
+           Data->ADCRSTSTCT0 = 0;
+           Data->ADCRSTENDCT0 = (uint16_t *)(Data->ADCRSTSTCT0 + ADC_Reset_ClockCycle);
+
+           Data->ADCRSTSTCT1 = (uint16_t *)( Data->ADCRSTSTCT0 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
+           Data->ADCRSTENDCT1 = (uint16_t *)( Data->ADCRSTSTCT1 + ADC_Reset_ClockCycle);
+           
+           Data->ADCRSTSTCT2 = (uint16_t *)( Data->ADCRSTSTCT1 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
+           Data->ADCRSTENDCT2 = (uint16_t *)( Data->ADCRSTSTCT2 + ADC_Reset_ClockCycle);
+           
+           Data->ADCRSTSTCT3 = (uint16_t *)( Data->ADCRSTSTCT2 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
+           Data->ADCRSTENDCT3 = (uint16_t *)( Data->ADCRSTSTCT3 + ADC_Reset_ClockCycle);
+
+        }
+   }
+
+    
 }
+
