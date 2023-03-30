87d672e88e39e4b76d1fcd8498c4d49786db16a7
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-03-30 17:37:44 +0200

    Extracted ADC timers init to ADC_TimersInit() function

--- AFE4400.c
+++ AFE4400.c
@@ -6,6 +6,7 @@
 
 AFE4400_Data_t AFE4400_Data;    /** AFE4400 All registers data */
 
+
 /** Software reset - resets all internal registers to the default values */
 static void SoftwareReset(AFE4400_Data_t *Data)
 {
@@ -29,25 +30,25 @@ static void TimerModuleInit( uint16_t PRF , uint8_t DutyCycle, AFE4400_Data_t *D
    {
         Data->PRPCOUNT = (uint16_t *)(AFE4400_CLOCK_FRQ / PRF - 1);
         Data->ADCRSTENDCT0 = Data->PRPCOUNT;
-
-
-        if ((0 < DutyCycle) && (100 > DutyCycle))
-        {
-           Data->ADCRSTSTCT0 = 0;
-           Data->ADCRSTENDCT0 = (uint16_t *)(Data->ADCRSTSTCT0 + ADC_Reset_ClockCycle);
-
-           Data->ADCRSTSTCT1 = (uint16_t *)( Data->ADCRSTSTCT0 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
-           Data->ADCRSTENDCT1 = (uint16_t *)( Data->ADCRSTSTCT1 + ADC_Reset_ClockCycle);
-           
-           Data->ADCRSTSTCT2 = (uint16_t *)( Data->ADCRSTSTCT1 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
-           Data->ADCRSTENDCT2 = (uint16_t *)( Data->ADCRSTSTCT2 + ADC_Reset_ClockCycle);
-           
-           Data->ADCRSTSTCT3 = (uint16_t *)( Data->ADCRSTSTCT2 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
-           Data->ADCRSTENDCT3 = (uint16_t *)( Data->ADCRSTSTCT3 + ADC_Reset_ClockCycle);
-
-        }
    }
 
+   ADC_TimersInit(PRF, DutyCycle, &Data);
+
     
+}
+
+static void ADC_TimersInit(uint8_t PRF, uint8_t DutyCycle, AFE4400_Data_t *Data)
+{
+    if ((0 != PRF) && (0 < DutyCycle) && (100 > DutyCycle))
+    {
+       Data->ADCRSTSTCT0 = 0;
+       Data->ADCRSTENDCT0 = (uint16_t *)(Data->ADCRSTSTCT0 + ADC_Reset_ClockCycle);
+       Data->ADCRSTSTCT1 = (uint16_t *)( Data->ADCRSTSTCT0 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
+       Data->ADCRSTENDCT1 = (uint16_t *)( Data->ADCRSTSTCT1 + ADC_Reset_ClockCycle);
+       Data->ADCRSTSTCT2 = (uint16_t *)( Data->ADCRSTSTCT1 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
+       Data->ADCRSTENDCT2 = (uint16_t *)( Data->ADCRSTSTCT2 + ADC_Reset_ClockCycle);
+       Data->ADCRSTSTCT3 = (uint16_t *)( Data->ADCRSTSTCT2 + ((AFE4400_CLOCK_FRQ / PRF) * (DutyCycle / 100)));
+       Data->ADCRSTENDCT3 = (uint16_t *)( Data->ADCRSTSTCT3 + ADC_Reset_ClockCycle);
+    }
 }
 
