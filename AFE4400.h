50b53a252583be32c0a41f3685e38c504bae3524
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-04-02 15:34:32 +0200

    Changed argument in Power down and up function declarotion

--- AFE4400.h
+++ AFE4400.h
@@ -7,10 +7,10 @@
 #define ADC_Reset_ClockCycle    3U
 
 /** AFE4400 Power down */
-void AFE4400_PowerDown(void);
+void AFE4400_PowerDown(AFE4400_Data_t *Data);
 
 /** AFE4400 Power Up */
-void AFE4400_PowerUp(void);
+void AFE4400_PowerUp(AFE4400_Data_t *Data);
 
 
 #endif
