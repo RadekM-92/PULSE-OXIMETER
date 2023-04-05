96a23a5247a76bbfa0fb1e313b323b2055384f61
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-04-05 20:12:30 +0200

    Added TxLedCurrentInit() definition

--- AFE4400.c
+++ AFE4400.c
@@ -400,7 +400,32 @@ static uint8_t LED_Current_mA_to_Raw(uint8_t LedCurrent)
     }
 }
 
+/** Tx LEDs current init
+ * Parameters - configuration parameters
+ * Data - AFE4400 registers
+*/
+void TxLedsCurrentInit(const AFE4400_Parameters_t *Parameters, AFE4400_Data_t *Data)
+{
+    const uint32_t LED1_Mask = 0xffff00ff;
+    const uint32_t LED2_Mask = 0xffffff00;
+    uint8_t LED1_CurrentRaw;
+    uint8_t LED2_CurrentRaw;
 
+    LED1_CurrentRaw = LED_Current_mA_to_Raw(Parameters->LED1_Current_mA);
+    LED2_CurrentRaw = LED_Current_mA_to_Raw(Parameters->LED2_Current_mA);
+    
+    if (0 != LED1_CurrentRaw && 0 != LED2_CurrentRaw)
+    {
+        Data->LEDCNTRL &= (LED1_Mask & LED2_Mask);
+        Data->LEDCNTRL |= ((LED1_CurrentRaw << 8) | LED2_CurrentRaw);
+        Data->LEDCNTRL |= (1 << LEDCUROFF);
+    }
+    else
+    {
+        Data->LEDCNTRL &= ~(1 << LEDCUROFF);
+    }
+
+}
 
 
 
