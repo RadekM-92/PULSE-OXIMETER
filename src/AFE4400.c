302e9793804fe7b367a8203c6e05af432de292e7
Radoslaw Marcinowski <rm92.git@gmail.com>
2024-03-27 08:18:56 +0100

    Added stm32f1xx_hal.h into AFE4400.c

--- src/AFE4400.c
+++ src/AFE4400.c
@@ -2,6 +2,14 @@
 #include "AFE4400.h"
 #include "AFE4400_Types.h"
 
+#ifndef STM32F103
+#define STM32F103
+#endif
+
+#ifdef STM32F103
+#include "stm32f1xx_hal.h"
+#endif
+
 AFE4400_Data_t AFE4400_Data = {0};                /** AFE4400 All registers data */
 AFE4400_Parameters_t AFE4400_Parameters = {0};    /** AFE4400 - Parameters */
 AFE4400_LEDs_RealDataADC_t AFE4400_LEDs = {0};    /** AFE4400 - LEDs real ADC Data */
