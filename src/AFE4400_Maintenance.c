a95a7434926ac1322291e05e91bebdd351604301
Radoslaw Marcinowski <rm92.git@gmail.com>
2024-04-04 19:56:03 +0200

    Added content to AFE4400_MemCmp

--- src/AFE4400_Maintenance.c
+++ src/AFE4400_Maintenance.c
@@ -9,5 +9,19 @@ uint8_t AFE4400_MemDump(AFE4400_Data_t *Data)
 
 uint8_t AFE4400_MemCmp(const AFE4400_Data_t *DataSend, AFE4400_Data_t *DataReceive, AFE4400_REGS_ADDRESS_t Address, uint8_t Size)
 {
+    uint8_t i;
+    uint8_t status = 0;
+    uint32_t *Tx = (uint32_t*) &DataSend->CONTROL0;
+    uint32_t *Rx = (uint32_t*) &DataReceive->CONTROL0;
 
+    for(i=Address; i<=Size; i++)
+    {
+        if (Tx[i] != Rx[i])
+        {
+            status = i;
+            break;
+        }    
+    }
+
+    return status;
 }
